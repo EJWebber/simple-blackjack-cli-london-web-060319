30db43fe7b6ea96891d438a539ec9303a309c7f4
Ed Webber <edwebber06@gmail.com>
2019-05-21 17:52:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,6 +32,9 @@ def hit?(current_total)
 prompt_user
 input = get_user_input
 until input == 'h'||input == 's'
+invalid_command
+prompt_user
+end
 if input == 'h'
   current_total += deal_card
 elsif input == 's'

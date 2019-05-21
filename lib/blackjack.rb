1b1029856068dcada76d417bfca78b3efba6e13e
Ed Webber <edwebber06@gmail.com>
2019-05-21 17:11:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,10 +6,10 @@ def deal_card
  (rand(1..11))
 end
 
-def display_card_total(card)
+def display_card_total(card_total)
   # code #display_card_total here
   
-  puts ("Your cards add up to #{card})
+  puts ("Your cards add up to #{card_total})
 end
 
 def prompt_user

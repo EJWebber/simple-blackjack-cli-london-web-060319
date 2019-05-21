684ec6238ba019595d06c6e376e06357cb2ff6bb
Ed Webber <edwebber06@gmail.com>
2019-05-21 17:39:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,7 +28,8 @@ end
 def initial_round
   # code #initial_round here
   card_total= deal_card + deal_card
-  return(display_card_total(card_total))
+  display_card_total(card_total)
+  return (card_total)
 end
 
 def hit?
